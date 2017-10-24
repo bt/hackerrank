46c96c4a8b1d6685e0150662cedeb0901fcf6a01
Bertram Truong <b@bertramtruong.com>
2017-10-24 17:09:16 +1100

    Update service_lane.py
    Separate logic groups.

--- algorithms/implementation/service_lane.py
+++ algorithms/implementation/service_lane.py
@@ -1,6 +1,8 @@
 n,t = input().strip().split(' ')
 n,t = [int(n),int(t)]
+
 width = [int(width_temp) for width_temp in input().strip().split(' ')]
+
 for a0 in range(t):
     i,j = input().strip().split(' ')
     i,j = [int(i),int(j)]
